bf06225862efc343d8ddee315a2eab89d50b2271
Tim Kuo <timytkuo@gmail.com>
2014-09-26 17:24:15 -0700

    check for existence of file before execute command

--- puppet/classes/play2.pp
+++ puppet/classes/play2.pp
@@ -12,6 +12,7 @@ class play2 {
 		path => "$execpath",
     	command => "wget -O /tmp/$activator.zip http://downloads.typesafe.com/typesafe-activator/1.2.10/typesafe-$activator.zip && cp /tmp/$activator.zip $usrdir/$activator.zip && touch $play2version",
 		require => [Class["linux"]],
+        creates => "$play2version",
         user => root,
 	}
 	
@@ -19,12 +20,14 @@ class play2 {
 		path => "$execpath",
     	command => "unzip -o $usrdir/$activator.zip && mv $usrdir/$activator $installdir/$activator && touch $play2unzip",
 		require => Exec["play2 download"],
+        creates => "$play2unzip",
         user => root,
 	}
 	
 	exec { "play2 link":
 		path => "$execpath",
     	command => "ln -s $installdir/$activator/activator /usr/local/bin/activator && touch $play2link",
+        creates => "$play2link",
 		require => Exec["play2 unzip"],
 	}
 
